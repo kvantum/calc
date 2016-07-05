503e0b0fca20c8825c96021140dd7c706ef3d483
kvantum <kvm_corp@ukr.net>
2016-07-05 13:52:36 +0300

    hey

---
+++ calc.py
@@ -0,0 +1,34 @@
+def input_numbers():
+    global num1, num2, operator, result
+    num1 = float(input('Enter the first number:'))
+    num2 = float(input('Enter the second number:'))
+    result = {'+': num1 + num2,
+              '-': num1 - num2,
+              '*': num1 * num2,
+              '/': num1 / num2
+              }
+    operator = raw_input('Choose the operation (+, -, *, /):')
+
+
+def calculate(number1, number2):
+    global result
+    result = {'+': number1 + number2,
+              '-': number1 - number2,
+              '*': number1 * number2,
+              '/': number1 / number2
+              }
+    print(str(number1) + ' ' + operator + ' ' + str(number2) + ' = ' + str(result[operator]))
+    return result
+
+input_numbers()
+while True:
+    if operator == '/' and num2 == 0:
+        print('Division by zero. Try again.')
+        print('')
+        input_numbers()
+    elif operator not in result:
+        print('Hey, You can use only these operators: +, -, *, /')
+        operator = raw_input('Choose the operation (+, -, *, /):')
+    else:
+        break
+calculate(num1, num2)
