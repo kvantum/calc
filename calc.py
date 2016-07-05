794487e725a27c2018fef83486929fc9c7ab17d7
kvantum <kvm_corp@ukr.net>
2016-07-05 23:26:08 +0300

    Checks for correct input

--- calc.py
+++ calc.py
@@ -1,31 +1,59 @@
 operators = ['+', '-', '*', '/']
 def input_numbers():
-    global num1, num2, operator, result
-    num1 = float(input('Enter the first number:'))
-    num2 = float(input('Enter the second number:'))
-    
+    """
+    Reads numbers and operator from the console. Converts numbers to float.
+    :return: list with 3 elements: two numbers and operator
+    """
+    print('------ This is the simple calculator -----------')
+    while True:
+        try:
+            num1 = float(input('Enter the first number:'))
+            break
+        except:
+            print('Try again. Enter a number, please.')
+
+    while True:
+        try:
+            num2 = float(input('Enter the second number:'))
+            break
+        except:
+            print('Try again. Enter a number, please.')
+
     operator = raw_input('Choose the operation (+, -, *, /):')
+    inp_data = [num1, num2, operator] #list with entered data
+    return inp_data
 
 
-def calculate(number1, number2):
-    global result
-    result = {'+': number1 + number2,
-              '-': number1 - number2,
-              '*': number1 * number2,
-              '/': number1 / number2
+def calculate(numb):
+    """
+    Calculates the result of the operation.
+    :param numb: a list with 3 elements: two numbers and operator
+    :return: result of the operation
+    """
+    oper = numb[2]
+    result = {'+': numb[0] + numb[1],
+              '-': numb[0] - numb[1],
+              '*': numb[0] * numb[1],
+              '/': numb[0] / numb[1]
               }
-    print(str(number1) + ' ' + operator + ' ' + str(number2) + ' = ' + str(result[operator]))
-    return result
+    print('Result of the operation:')
+    res = str(numb[0]) + ' ' + oper + ' ' + str(numb[1]) + ' = ' \
+          + str(result[oper])
+    print(res)
+    return result[oper]
 
-input_numbers()
+inp_data = input_numbers()
+
+# Check if the correct operation entered.
 while True:
-    if operator == '/' and num2 == 0:
+    if inp_data[2] == '/' and inp_data[1] == 0:
         print('Division by zero. Try again.')
         print('')
         input_numbers()
-    elif operator not in operators:
-        print('Hey, You can use only these operators: +, -, *, /')
-        operator = raw_input('Choose the operation (+, -, *, /):')
+    elif inp_data[2] not in operators:
+        print('Hey, You can use only these operations: +, -, *, /')
+        inp_data[2] = raw_input('Choose the operation (+, -, *, /):')
     else:
         break
-calculate(num1, num2)
+
+calculate(inp_data)
